6882e39555aa3a58cd1affb2745101aec763866d
Muhammad Farid <mfarid2121@gmail.com>
2024-05-01 19:38:58 +0700

    fix: revert test code direct to form user

--- src/tubes/Tubes.java
+++ src/tubes/Tubes.java
@@ -21,8 +21,8 @@ public class Tubes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        FormLoginRegister frame = new FormLoginRegister();
-        FormUser frame = new FormUser();
+        FormLoginRegister frame = new FormLoginRegister();
+//        FormUser frame = new FormUser();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
